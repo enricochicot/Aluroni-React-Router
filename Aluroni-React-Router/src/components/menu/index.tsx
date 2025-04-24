92623c41dc55ecf878d7460bf5b0ea985ce06aa9
Enrico <enrico.leite@pca.com.br>
2025-04-24 15:16:10 -0300

    Criação de menu, footer, notfound, estilização e demais telas. Código necessita ser revisado, não está funcional.

--- Aluroni-React-Router/src/components/menu/index.tsx
+++ Aluroni-React-Router/src/components/menu/index.tsx
@@ -3,27 +3,23 @@ import styles from "./Menu.module.scss";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
-  const rotas = [
-    {
-      label: "Início",
-      to: "/",
-    },
-    {
-      label: "Cardápio",
-      to: "/cardapio",
-    },
-    {
-      label: "Sobre",
-      to: "/sobre",
-    },
-  ];
+  const rotas = [{
+    label: 'Início',
+    to: '/'
+  }, {
+    label: 'Cardápio',
+    to: '/cardapio'
+  }, {
+    label: 'Sobre',
+    to: '/sobre'
+  }];
   return (
     <nav className={styles.menu}>
       <Logo />
       <ul className={styles.menu__list}>
         {rotas.map((rota, index) => (
           <li key={index} className={styles.menu__link}>
-            <Link to={rota.to} className={styles.menu__link}>
+            <Link to={rota.to}>
               {rota.label}
             </Link>
           </li>
